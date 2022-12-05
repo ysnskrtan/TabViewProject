5a128aef23ba0c6642cf2be15b97f7af1bce0fa7
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-12-05 12:35:19 +0300

    Initial Commit

---
+++ TabViewProject/TabViewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewProjectApp.swift
+//  TabViewProject
+//
+//  Created by Yasin Şükrü Tan on 5.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TabViewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
